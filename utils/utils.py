bea4fa65819b6bb5fae047272a6bbda0babba06a
Mateusz Borowski <m.borowski128@gmail.com>
2022-07-08 12:03:09 +0200

    Add data loading

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
 import torch
+import torchvision
+
+from utils.transforms import train_transform, eval_transform
 
 
 def get_targets(dataset):
@@ -12,3 +15,46 @@ def get_targets(dataset):
         targets[i] = label
 
     return targets
+
+
+def get_train_dataset():
+    return torchvision.datasets.GTSRB(
+        root='./data', split='train', download=True, transform=train_transform)
+
+
+def get_eval_dataset():
+    return torchvision.datasets.GTSRB(
+        root='./data', split='test', download=True, transform=eval_transform)
+
+
+def get_loaders(batch_size=128):
+    generator = torch.Generator().manual_seed(42)
+
+    train_dataset = get_train_dataset()
+    eval_dataset = get_eval_dataset()
+
+    val_dataset, test_dataset = torch.utils.data.random_split(
+        eval_dataset,
+        [len(eval_dataset)//2, len(eval_dataset)//2], generator=)generator
+
+    targets = get_targets(train_dataset)
+    _, counts = targets.unique(return_counts=True)
+
+    # dataset is imbalanced, so WeightedRandomSampler is used to mitigate that imbalance
+    weights = 1 / counts.float()
+    sample_weights = weights[targets.long()]
+    sampler = torch.utils.data.WeightedRandomSampler(
+        weights=sample_weights,
+        num_samples=len(sample_weights),
+        replacement=True,
+        generator=generator
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=batch_size, sampler=sampler)
+    val_loader = torch.utils.data.DataLoader(
+        dataset=val_dataset, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=batch_size)
+
+    return train_loader, val_loader, test_loader
